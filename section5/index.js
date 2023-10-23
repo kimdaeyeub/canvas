c6995cff873073b63a40dac85e8dd01784bd034e
kimdaeyeub <kdy3915@naver.com>
2023-10-24 00:53:00 +0900

    update

--- section5/index.js
+++ section5/index.js
@@ -13,10 +13,11 @@ class Root {
     this.speedY = Math.random() * 4 - 2;
     this.maxSize = Math.random() * 7 + 5;
     this.size = Math.random() * 1 + 2;
+    this.angle = Math.random() * 6.2;
   }
 
   update() {
-    this.x += this.speedX;
+    this.x += this.speedX + Math.sin(this.angle);
     this.y += this.speedY;
     this.size += 0.1;
     if (this.size < this.maxSize) {
@@ -25,6 +26,7 @@ class Root {
       ctx.fillStyle = "hsl(140,100%,50%)";
       ctx.fill();
       ctx.stroke();
+      requestAnimationFrame(this.update.bind(this));
     }
   }
 }
